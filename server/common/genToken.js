18783907ad13b88dacfa3cf084046be2b718082f
Shashank Sahay <sahayshashank28@gmail.com>
2025-03-16 13:04:26 +0000

    Fix of #8 JWT token fix
    This commit has the following change:
    1. Secret in env should be JWT_SECRET not SECRET_KEY

--- server/common/genToken.js
+++ server/common/genToken.js
@@ -1,8 +1,8 @@
 const jwt = require("jsonwebtoken")
 
 // Function generates a token with a certain expiry duration
-function generateToken(user, secretKey, expire = "1h") {
-    const token = jwt.sign(user, secretKey, { expiresIn: expire })
+function generateToken(user, expire = "1h") {
+    const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: expire })
     return token
 }
 
